cf397d320d9de5de08ae1aef4c7f0114bcece34c
Seito Horiguchi <seito.horiguchi@gmail.com>
2024-08-03 10:30:48 +0900

    build: OK

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import { defineConfig } from "vite"
 export default defineConfig({
   base: "/api-test",
   build: {
-    
-    // root: "/api-test",
     assetsDir: "./assets",
-    // rollupOptions: {
-      
-    // },
   },
 })
